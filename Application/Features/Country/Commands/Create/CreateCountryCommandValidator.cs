b20b461b59b6450e91dfa344834a1bbb77f2206f
foadelamoury <foad.osama@hotmail.com>
2023-01-03 10:42:59 +0200

    Using Fluent Validation
    Starting to fill fluent validation

--- Application/Features/Country/Commands/Create/CreateCountryCommandValidator.cs
+++ Application/Features/Country/Commands/Create/CreateCountryCommandValidator.cs
@@ -1,6 +1,17 @@
-﻿namespace Application.Features.Country.Commands.Create
+﻿using FluentValidation;
+
+namespace Application.Features.Country.Commands.Create
 {
-    public class CreateCountryCommandValidator
+    public class CreateCountryCommandValidator : AbstractValidator<CreateCountryCommand>
     {
+        public CreateCountryCommandValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty();
+            RuleFor(x => x.NameA).NotEmpty().WithMessage("Please specify a Country Name")
+       .MaximumLength((int)100).WithMessage("Maximum length is 100 letter");
+
+        }
+
+
     }
 }
