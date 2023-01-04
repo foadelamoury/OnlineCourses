5e6e6332064feefc1ecd7e5648c5dc274bd30247
Foad Elamoury <foad.osama@hotmail.com>
2023-01-04 20:00:48 +0200

    s3

--- Application/Features/Country/Commands/Create/CreateCountryCommandValidator.cs
+++ Application/Features/Country/Commands/Create/CreateCountryCommandValidator.cs
@@ -7,7 +7,7 @@ namespace Application.Features.Country.Commands.Create
         public CreateCountryCommandValidator()
         {
             RuleFor(x => x.Id).NotEmpty();
-            RuleFor(x => x.NameA).NotEmpty().WithMessage("Please specify a Country Name")
+            RuleFor(x => x.NameA).NotEmpty().WithMessage("ادخل البلد")
        .MaximumLength((int)100).WithMessage("Maximum length is 100 letter");
 
         }
